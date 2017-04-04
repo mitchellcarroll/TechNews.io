d3a4919ee1ff0a9e5ba1c82e7cfa70c2d0b480f6
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-04-03 22:32:34 -0400

    4/3/17

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer'
-import App from './App';
+import { shallow } from 'enzyme';
+import App, { Search, Button, Table } from './App';
+
 
 describe('App', () => {
 
@@ -18,4 +20,64 @@ test('snapshots', () => {
   expect(tree).toMatchSnapshot();
 });
 
+});
+
+describe('Search', () => {
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+  });
+
+  test('sanpshots', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Button', () => {
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Give Me More</Button>, div;)
+  });
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Button>Give Me More</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z'},
+    ],
+  };
+
+
+
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+    <Table { ...props } />, div);
+  );
+  expect(element.find('.table-row').length).toBe(2);
+  });
 });
